75747efe0525b31c7d9fecd70753ff2040d0f07b
Akmikze <Asermaca@DESKTOP-20U1MNN>
2025-04-20 23:53:21 -0400

    Se añadieron nuevas validaciones para evitar inconvenientes con las entradas.
    New validations were added in order to avoid inconvenients with the inputs,

--- OldPhoneNumPad/Program.cs
+++ OldPhoneNumPad/Program.cs
@@ -8,7 +8,7 @@ public static class Program
     // Welcome and Exit Messages / Mensajes de bienvenida y despedida
     private const string WelcomeMessage = "Welcome to the Old Phone Number Pad Experience / Bienvenido a la Experiencia de escribir en un Pad Numerico Viejo";
     private const string ExitMessage = "Thanks for using the Old Phone Number Pad Experience / Gracias por usar la Experiencia de escribir en un Pad Numerico Viejo";
-    private const string InvalidInputMessage = "Remember to enter Valid Data (Numbers, Spaces, 0 with other numbers and one # are valid, letters, 0#, #, *# or any number without # are not valid) / Recuerda usar datos validos (Numeros, Espacio 0 con otros numeros y un # son validos, 0#, #, *# or cualquier otro numero sin # no son validos)";
+    private const string InvalidInputMessage = "Remember to enter Valid Data / Recuerda usar datos validos";
     private const string ReminderMessage = "Please type a Valid Input, TEST to see the test cases or Type Q if you want to exit / Porfavor ingresa datos validos, ingresa TEST para ver los casos de uso o Q si quieres salir del programa";
 
     public static void Main()
@@ -67,7 +67,7 @@ public static class Program
             }
             else // If the input is invalid / Si la entrada es invalida
             {
-                Console.WriteLine($"Input: {input} is invalid."); // We show the invalid input message / Mostramos el mensaje de entrada invalida
+                Console.WriteLine($"Input: {input} is invalid. | {expectedOutput}"); // We show the invalid input message / Mostramos el mensaje de entrada invalida
             }
         }
         catch (Exception ex) // In case we have an error processing the input / En caso de que tengamos un error procesando la entrada. 
@@ -82,13 +82,15 @@ public static class Program
         RunTestCase("227*#", "B");
         RunTestCase("4433555 555666#", "HELLO");
         RunTestCase("8 88777444666*664#", "TURING");
-        RunTestCase("344477733222833 3022999092777 777336607776662244466338 8#", "DIRECTED BY WARREN ROBINETT"); 
-        RunTestCase("7**", "ERROR: YOU DIDN'T USE #");
+        RunTestCase("344477733222833 3022999092777 777336607776662244466338 8#", "DIRECTED BY WARREN ROBINETT");
+        RunTestCase("7", "ERROR: YOU DIDN'T USE #");
+        RunTestCase("7**#", "ERROR: YOU CAN'T USE TWO ASTERISK ONE AFTER THE OTHER");
         RunTestCase("0#", "ERROR: YOU ONLY ENTER A SPACE");
-        RunTestCase("#", "ERROR: YOU DIDN'T ENTER ANYTHING");
+        RunTestCase("*#", "ERROR: YOU DIDN'T ENTER ANYTHING");
         RunTestCase("#", "ERROR: YOU DIDN'T ENTER ANYTHING");
         RunTestCase("7##", "ERROR: TOO MANY #");
         RunTestCase("7#7#7#", "ERROR: TOO MANY #");
+        RunTestCase("A#", "ERROR: ONLY NUMBERS ARE ALLOWED ON THE INPUT");
         RunTestCase("255644455999933#", "AKMIKZE");
     }
     private static bool IsValidInput(string input) // Input Validations / Validaciones de entrada
@@ -97,8 +99,10 @@ public static class Program
                       Regex.IsMatch(input, @"^[0-9#* ]+$") &&
                       input.Length > 1 &&
                       input.Count(c => c == '#') == 1 &&
-                      input != "0#" && input != "*#"; 
-        
+                      !input.Contains("**") &&
+                      !Regex.IsMatch(input, @"\*{3,}") &&
+                      input != "0#" && input != "*#";
         // We limit the input to only be from 0 to 9, # and * and to only have one # at the end, to have more than 1 character and to not be 0# or *# / Limitamos la entrada a solo ser de 0 a 9, # y * y tener solo un # al final, tener mas de 1 caracter y no ser 0# o *#
+
     }
 }
